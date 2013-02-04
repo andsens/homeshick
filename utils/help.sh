a56bc08781e0d78cdefa3324f36347b4dcd36caf
Anders Ingemann <anders@ingemann.de>
2013-02-04 22:45:19 +0100

    Allow multiple inputs to subcommands

--- utils/help.sh
+++ utils/help.sh
@@ -8,12 +8,12 @@ cat <<EOM
  Usage: homesick [options] TASK
 
  Tasks:
-  homesick clone URI          # Clone URI as a castle for homeshick
-  homesick generate PATH      # generate a homeshick-ready git repo at PATH
+  homesick clone [URI..]      # Clone URI as a castle for homeshick
+  homesick generate [PATH..]  # generate a homeshick-ready git repo at PATH
   homesick list               # List cloned castles
-  homesick check CASTLE       # Check a castle for updates
-  homesick pull NAME          # Update the specified castle
-  homesick symlink NAME       # Symlinks all dotfiles from the specified castle
+  homesick check [CASTLE..]   # Check a castle for updates
+  homesick pull [CASTLE..]    # Update the specified castle
+  homesick symlink [CASTLE..] # Symlinks all dotfiles from the specified castle
   homesick track FILE CASTLE  # add a file to a castle
   homesick help [TASK]        # Describe available tasks or one specific task
  
@@ -26,12 +26,12 @@ EOM
 
 function extended_help {
 	case $1 in
-		clone)    printf "Usage:\n  homesick clone URL"          ;;
-		generate) printf "Usage:\n  homesick generate PATH"      ;;
+		clone)    printf "Usage:\n  homesick clone [URL..]"      ;;
+		generate) printf "Usage:\n  homesick generate [PATH..]"  ;;
 		list)     printf "Usage:\n  homesick list"               ;;
-		check)    printf "Usage:\n  homesick check CASTLE"       ;;
-		pull)     printf "Usage:\n  homesick pull NAME"          ;;
-		symlink)  printf "Usage:\n  homesick symlink NAME"       ;;
+		check)    printf "Usage:\n  homesick check [CASTLE..]"   ;;
+		pull)     printf "Usage:\n  homesick pull [CASTLE..]"    ;;
+		symlink)  printf "Usage:\n  homesick symlink [CASTLE..]" ;;
 		track)    printf "Usage:\n  homesick track FILE CASTLE"  ;;
 		help)     printf "Usage:\n  homesick help [TASK]"        ;;
 		*)    help  ;;
