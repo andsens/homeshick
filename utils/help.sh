7d0bb1beab1ad5c275b30c8a0cc9e1c1f84073c5
Matt Klich <matt@elementalvoid.com>
2013-03-09 08:16:17 -0700

    Add link as alias to symlink. Update help for link and updates

--- utils/help.sh
+++ utils/help.sh
@@ -19,6 +19,10 @@ It is a bash stand-in for the original homesick by technicalpickles.
   homesick track CASTLE [FILE..] # Add a file to a castle
   homesick help [TASK]           # Show usage of a task
 
+ Aliases:
+  link    # Alias to symlink
+  updates # Alias to check
+
  Runtime options:
    -q, [--quiet]    # Suppress status output
    -s, [--skip]     # Skip files that already exist
@@ -42,8 +46,10 @@ function extended_help {
 		generate) printf "Usage:\n  homesick generate [PATH..]"     ;;
 		list)     printf "Usage:\n  homesick list"                  ;;
 		check)    printf "Usage:\n  homesick check [CASTLE..]"      ;;
+    updates)  printf "Usage:\n  homesick updates [CASTLE..]"      ;;
 		pull)     printf "Usage:\n  homesick pull [CASTLE..]"       ;;
 		symlink)  printf "Usage:\n  homesick symlink [CASTLE..]"    ;;
+    link)     printf "Usage:\n  homesick link [CASTLE..]"    ;;
 		track)    printf "Usage:\n  homesick track CASTLE [FILE..]" ;;
 		help)     printf "Usage:\n  homesick help [TASK]"           ;;
 		*)    help  ;;
