58b2f5dc1c74fd592f0a7f5886d54696eadc3c10
Anders Ingemann <anders@ingemann.de>
2013-03-11 21:02:28 +0100

    Make some parameters non-optional. Also provide help when obligatory parameters aren't specified. Remove the square brackets for obligatory parameters from the help menu.

--- utils/help.sh
+++ utils/help.sh
@@ -10,13 +10,13 @@ It is a bash stand-in for the original homesick by technicalpickles.
  Usage: homesick [options] TASK
 
  Tasks:
-  homesick clone [URI..]         # Clone URI as a castle for homesick
-  homesick generate [PATH..]     # Generate a homesick-ready git repo at PATH
+  homesick clone URI..           # Clone URI as a castle for homesick
+  homesick generate PATH..       # Generate a homesick-ready git repo at PATH
   homesick list                  # List cloned castles
   homesick check [CASTLE..]      # Check a castle for updates
   homesick pull [CASTLE..]       # Update a castle
   homesick symlink [CASTLE..]    # Symlinks all dotfiles from a castle
-  homesick track CASTLE [FILE..] # Add a file to a castle
+  homesick track CASTLE FILE..   # Add a file to a castle
   homesick help [TASK]           # Show usage of a task
 
  Aliases:
@@ -42,15 +42,15 @@ function help_err {
 
 function extended_help {
 	case $1 in
-		clone)    printf "Usage:\n  homesick clone [URL..]"         ;;
-		generate) printf "Usage:\n  homesick generate [PATH..]"     ;;
+		clone)    printf "Usage:\n  homesick clone URL.."         ;;
+		generate) printf "Usage:\n  homesick generate PATH.."     ;;
 		list)     printf "Usage:\n  homesick list"                  ;;
 		check)    printf "Usage:\n  homesick check [CASTLE..]"      ;;
     updates)  printf "Usage:\n  homesick updates [CASTLE..]"      ;;
 		pull)     printf "Usage:\n  homesick pull [CASTLE..]"       ;;
 		symlink)  printf "Usage:\n  homesick symlink [CASTLE..]"    ;;
     link)     printf "Usage:\n  homesick link [CASTLE..]"    ;;
-		track)    printf "Usage:\n  homesick track CASTLE [FILE..]" ;;
+		track)    printf "Usage:\n  homesick track CASTLE FILE.." ;;
 		help)     printf "Usage:\n  homesick help [TASK]"           ;;
 		*)    help  ;;
 		esac
