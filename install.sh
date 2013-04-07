d0d306522469986cc8458515adf208f5d9440428
Anders Ingemann <anders@ingemann.de>
2013-04-07 17:50:22 +0200

    Simplify install script and exit on any error

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-pushd $HOME > /dev/null
-repos='.homesick/repos'
+set -e
+repos=$HOME/.homesick/repos
 mkdir -p $repos
-git clone git://github.com/andsens/homeshick.git .homesick/repos/homeshick
+git clone git://github.com/andsens/homeshick.git $repos/homeshick
 ln -s $repos/homeshick/home/.homeshick $HOME/.homeshick
-popd > /dev/null
