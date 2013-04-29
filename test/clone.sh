e18ae93c1cfc49f307c8d9b3c947eb530968919e
Anders Ingemann <anders@ingemann.de>
2013-04-29 18:55:21 +0200

    Use bash for testing instead of sh

--- test/clone.sh
+++ test/clone.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-testCloning() {
+function testCloning() {
 	$HOMESHICK_BIN --batch clone $REPO_FIXTURES/rc-files > /dev/null
 	assertSame "\`clone' did not exit with status 0" 0 $?
 }
 
-tearDown() {
+function tearDown() {
 	rm -rf "$HOMESICK/repos/rc-files"
 }
 
--- test/link.sh
+++ test/link.sh
@@ -1,16 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-oneTimeSetUp() {
+function oneTimeSetUp() {
 	$HOMESHICK_BIN --batch clone $REPO_FIXTURES/rc-files > /dev/null
 }
 
-testLinking() {
+function testLinking() {
 	assertFalse "The .bashrc file existed before symlinking" "[ -e $HOME/.bashrc ]"
 	$HOMESHICK_BIN --batch link rc-files > /dev/null
 	assertTrue "\`link' did not symlink the .bashrc file" "[ -e $HOME/.bashrc ]"
 }
 
-oneTimeTearDown() {
+function oneTimeTearDown() {
 	rm -rf "$HOMESICK/repos/rc-files"
 	find "$HOME" -depth 1 -not -name '.homesick' -not -name '.homeshick' -exec rm -rf {} \;
 }
