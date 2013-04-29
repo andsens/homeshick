a7eb70b25a78d2aa97aa6608689055cab74ef5a2
Anders Ingemann <anders@ingemann.de>
2013-04-29 22:00:46 +0200

    Simplify install script

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-set -e
-repos=$HOME/.homesick/repos
-mkdir -p $repos
-git clone git://github.com/andsens/homeshick.git $repos/homeshick
-ln -s $repos/homeshick/home/.homeshick $HOME/.homeshick
+git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+$HOME/.homesick/repos/homeshick/home/.homeshick link
