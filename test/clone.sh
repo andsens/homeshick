2096552125db551e8a29bba810d15d2590d216ca
Anders Ingemann <anders@ingemann.de>
2013-04-30 02:43:34 +0200

    Test the symlink prompt by using expect.

--- test/clone.sh
+++ test/clone.sh
@@ -5,7 +5,20 @@ function testCloning() {
 	assertSame "\`clone' did not exit with status 0" 0 $?
 }
 
+function testSymlinkPrompt() {
+	cat <<EOF | expect -f - > /dev/null
+		spawn $HOMESHICK_BIN clone $REPO_FIXTURES/rc-files
+		expect "*clone*$REPO_FIXTURES/rc-files"
+		expect "*updates*The castle rc-files has new files."
+		expect "symlink?*\[yN\]"
+		send "y\r"
+		expect EOF
+EOF
+	assertTrue 'bashrc symlinked after prompt' "[ -f $HOME/.bashrc ]"
+}
+
 function tearDown() {
+	rm -f "$HOME/.bashrc"
 	rm -rf "$HOMESICK/repos/rc-files"
 }
 
