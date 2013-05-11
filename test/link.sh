3da02b37be77f27c7975e8520cf02469736573aa
Anders Ingemann <anders@ingemann.de>
2013-05-11 13:47:48 +0200

    Merge deep_linking and link tests

--- test/link.sh
+++ test/link.sh
@@ -2,6 +2,7 @@
 
 function oneTimeSetUp() {
 	$HOMESHICK_BIN --batch clone $REPO_FIXTURES/rc-files > /dev/null
+	$HOMESHICK_BIN --batch clone $REPO_FIXTURES/dotfiles > /dev/null
 }
 
 function testLinking() {
@@ -10,9 +11,44 @@ function testLinking() {
 	assertTrue "\`link' did not symlink the .bashrc file" "[ -e $HOME/.bashrc ]"
 }
 
+function testDeepLinking() {
+	mkdir -p $HOME/.config/bar.dir
+	cat > $HOME/.config/foo.conf <<EOF
+#I am just a regular foo.conf file 
+[foo]
+A=True
+EOF
+	cat > $HOME/.config/bar.dir/bar.conf <<EOF
+#I am just a regular bar.conf file 
+[bar]
+A=True
+EOF
+	
+	assertTrue "The .config/foo.conf file did not exist before symlinking" "[ -f $HOME/.config/foo.conf ]"
+	#.config/foo.conf should be overwritten by a directory of the same name
+	assertTrue "The .config/bar.dir/ directory did not exist before symlinking" "[ -d $HOME/.config/bar.dir ]"
+	#.config/bar.dir should be overwritten by a file of the same name
+	$HOMESHICK_BIN --batch --force link dotfiles > /dev/null
+	assertTrue "'link' did not symlink the .config/foo.conf directory" "[ -d $HOME/.config/foo.conf ]"
+	assertTrue "'link' did not symlink the .config/bar.dir directory" "[ -f $HOME/.config/bar.dir ]"
+}
+
+function testLegacySymlinks() {
+	assertTrue "known_hosts file does not exist" "[ -e $HOME/.ssh/known_hosts ]"
+	rm -rf "$HOME/.ssh"
+	# Recreate the legacy scenario
+	ln -s $HOMESICK/repos/dotfiles/home/.ssh $HOME/.ssh
+	$HOMESHICK_BIN --batch --force link dotfiles > /dev/null
+	# Without legacy handling if we were to run `file $HOME/.ssh/known_hosts` we would get
+	# .ssh/known_hosts: symbolic link in a loop
+	# The `test -e` is sufficient though
+	assertTrue "known_hosts file is a symbolic loop or does not exist" "[ -e $HOME/.ssh/known_hosts ]"
+}
+
 function oneTimeTearDown() {
 	rm -rf "$HOMESICK/repos/rc-files"
-	find "$HOME" -depth 1 -not -name '.homesick' -not -name '.homeshick' -exec rm -rf {} \;
+	rm -rf "$HOMESICK/repos/dotfiles"
+	find "$HOME" -mindepth 1 -not -name '.homesick' -not -name '.homeshick' -not -name '.gitconfig' -delete 
 }
 
 source $SHUNIT2
