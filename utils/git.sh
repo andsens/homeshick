97b3818456847bce1723073c2da2223489080ae3
Anders Ingemann <anders@ingemann.de>
2013-08-29 00:09:07 +0200

    Use upstream url, not origin url, when checking Fixes #10

--- utils/git.sh
+++ utils/git.sh
@@ -104,8 +104,9 @@ function check {
 	pending 'checking' $castle
 	castle_exists 'check' $castle
 
-	local ref=$(cd $repo; git symbolic-ref HEAD 2>/dev/null)
-	local remote_url=$(cd $repo; git config remote.origin.url 2>/dev/null)
+	local ref=$(cd $repo; git symbolic-ref --short HEAD 2>/dev/null)
+	local remote_name=$(cd $repos/$reponame; git config branch.$ref.remote 2>/dev/null)
+	local remote_url=$(cd $repo; git config remote.$remote_name.url 2>/dev/null)
 	local remote_head=$(git ls-remote -q --heads "$remote_url" "$ref" 2>/dev/null | cut -f 1)
 	if [[ $remote_head ]]; then
 		local local_head=$(cd $repo; git rev-parse HEAD)
