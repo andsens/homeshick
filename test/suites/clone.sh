1713a2731bd5d0eb23aeaeeecd230e0c7c3f283a
Anders Ingemann <anders@ingemann.de>
2013-08-28 23:11:16 +0200

    More precise symlink prompt unit test

--- test/suites/clone.sh
+++ test/suites/clone.sh
@@ -11,15 +11,19 @@ function testCloning() {
 }
 
 function testSymlinkPrompt() {
+	open_bracket="\\u005b"
+	close_bracket="\\u005d"
+	esc="\\u001b$open_bracket"
 	cat <<EOF | expect -f - > /dev/null
 		spawn $HOMESHICK_BIN clone $REPO_FIXTURES/rc-files
-		expect "*clone*$REPO_FIXTURES/rc-files"
-		expect "*updates*The castle rc-files has new files."
-		expect "symlink?*\[yN\]"
-		send "y\r"
+		expect -ex "${esc}1;36m        clone${esc}0m $REPO_FIXTURES/rc-files\r${esc}1;32m        clone${esc}0m $REPO_FIXTURES/rc-files\r
+${esc}1;37m      updates${esc}0m The castle rc-files has new files.\r
+${esc}1;36m     symlink?${esc}0m ${open_bracket}yN${close_bracket} " {} default {exit 1}
+		send "y\n"
 		expect EOF
 EOF
-	assertTrue 'bashrc symlinked after prompt' "[ -f $HOME/.bashrc ]"
+	assertEquals "Failed verifying the clone command output." 0 $?
+	assertTrue "bashrc not symlinked after prompt" "[ -f $HOME/.bashrc ]"
 	rm -f "$HOME/.bashrc"
 	rm -rf "$HOMESICK/repos/rc-files"
 }
