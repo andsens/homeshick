9cccf121d7e6930ec44326ef250e214c3dac30d1
Jeremy Lin <jjlin@cs.stanford.edu>
2013-09-16 00:30:46 -0700

    Move 'home/.homeshick' to 'bin/homeshick'
    Also, move the helper scripts and update various paths as necessary.

--- bin/homeshick.sh
+++ bin/homeshick.sh
@@ -1,6 +1,6 @@
 # This helper script should be sourced via an alias, e.g.
 #
-#   alias homeshick="source $HOME/.homesick/repos/homeshick/home/.homeshick.sh"
+#   alias homeshick="source $HOME/.homesick/repos/homeshick/bin/homeshick.sh"
 #
 # Use portable syntax to accommodate as many Bourne-family shells as possible.
 if [ "$1" = "cd" ] && [ -n "$2" ]; then
@@ -10,5 +10,5 @@ if [ "$1" = "cd" ] && [ -n "$2" ]; then
         cd $HOME/.homesick/repos/$2
     fi
 else
-    $HOME/.homesick/repos/homeshick/home/.homeshick "$@"
+    $HOME/.homesick/repos/homeshick/bin/homeshick "$@"
 fi
