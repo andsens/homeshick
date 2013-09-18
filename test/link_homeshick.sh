5116a6c000597ddba2eee80101113413a9ea7527
Anders Ingemann <anders@ingemann.de>
2013-09-18 18:20:37 +0200

    Fix setup_homeshick() in the unit test setup

--- test/link_homeshick.sh
+++ test/link_homeshick.sh
@@ -3,6 +3,6 @@
 function setup_homeshick {
 	local hs_repo=$HOMESICK/repos/homeshick
 	mkdir -p $hs_repo
-	ln -s $(cd $SCRIPTDIR/../home; printf "$(pwd)") $hs_repo/home
+	ln -s $(cd $SCRIPTDIR/../bin; printf "$(pwd)") $hs_repo/bin
 	ln -s $(cd $SCRIPTDIR/../utils; printf "$(pwd)") $hs_repo/utils
 }
