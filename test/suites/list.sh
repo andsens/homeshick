c6c3899bbf8d32ceaa01127ecbcafc88ac188774
Anders Ingemann <anders@ingemann.de>
2013-12-12 00:07:35 +0100

    Fix list command to work with spaces in repo name

--- test/suites/list.sh
+++ test/suites/list.sh
@@ -8,13 +8,15 @@ function testList() {
 	$HOMESHICK_FN --batch clone $REPO_FIXTURES/rc-files > /dev/null
 	$HOMESHICK_FN --batch clone $REPO_FIXTURES/dotfiles > /dev/null
 	$HOMESHICK_FN --batch clone $REPO_FIXTURES/module-files > /dev/null
+	$HOMESHICK_FN --batch clone "$REPO_FIXTURES/repo with spaces in name" > /dev/null
 	esc="\\u001b\\u005b"
 	if $EXPECT_INSTALLED; then
 		cat <<EOF | expect -f - > /dev/null
 			spawn $HOMESHICK_BIN list
 			expect -ex "${esc}1;37m     dotfiles${esc}0m $REPO_FIXTURES/dotfiles\r
 ${esc}1;37m module-files${esc}0m $REPO_FIXTURES/module-files\r
-${esc}1;37m     rc-files${esc}0m $REPO_FIXTURES/rc-files\r\n" {} default {exit 1}
+${esc}1;37m     rc-files${esc}0m $REPO_FIXTURES/rc-files\r
+${esc}1;37mrepo with spaces in name${esc}0m $REPO_FIXTURES/repo with spaces in name\r\n" {} default {exit 1}
 EOF
 	else
 		startSkipping
@@ -24,6 +26,23 @@ EOF
 	rm -rf "$HOMESICK/repos/rc-files"
 	rm -rf "$HOMESICK/repos/dotfiles"
 	rm -rf "$HOMESICK/repos/module-files"
+	rm -rf "$HOMESICK/repos/repo with spaces in name"
+}
+
+function testListWithSpacesInRepoName() {
+	$HOMESHICK_FN --batch clone "$REPO_FIXTURES/repo with spaces in name" > /dev/null
+	esc="\\u001b\\u005b"
+	if $EXPECT_INSTALLED; then
+		cat <<EOF | expect -f - > /dev/null
+			spawn $HOMESHICK_BIN list
+			expect -ex "${esc}1;37mrepo with spaces in name${esc}0m $REPO_FIXTURES/repo with spaces in name\r\n" {} default {exit 1}
+EOF
+	else
+		startSkipping
+	fi
+	assertEquals "Failed verifying the list command output." 0 $?
+
+	rm -rf "$HOMESICK/repos/repo with spaces in name"
 }
 
 function testListAlteredUpstreamRemoteName() {
--- utils/git.sh
+++ utils/git.sh
@@ -92,13 +92,13 @@ function pull {
 }
 
 function list {
-	for reponame in $(list_castle_names); do
+	while IFS= read -d $'\n' -r reponame ; do
 		local ref=$(cd "$repos/$reponame"; git symbolic-ref HEAD 2>/dev/null)
 		local branch=${ref#refs/heads/}
 		local remote_name=$(cd "$repos/$reponame"; git config branch.$branch.remote 2>/dev/null)
 		local remote_url=$(cd "$repos/$reponame"; git config remote.$remote_name.url 2>/dev/null)
 		info "$reponame" "$remote_url"
-	done
+	done < <(list_castle_names)
 	return $EX_SUCCESS
 }
 
