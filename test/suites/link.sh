andsens/homeshick

commit 5a45ce8de85d8250924175802d19c8b1540b20b5

Fix bug when linking paths with spaces Added file with spaces in path to the repo fixture

utils/fs.sh
@@ -9,7 +9,7 @@ function symlink {
 		ignore 'ignored' "$castle"
 		return $EX_SUCCESS
 	fi
-	for remote in $(find $repo/home -mindepth 1 -name .git -prune -o -print); do
+	while IFS= read -d $'\0' -r remote ; do
 		filename=${remote#$repo/home/}
 		local=$HOME/$filename
 
@@ -19,10 +19,10 @@ function symlink {
 				# $local symlinks to $remote.
 				if [[ -d $remote && ! -L $remote ]]; then
 					# If $remote is a directory -> legacy handling.
-					rm $local
+					rm "$local"
 				else
 					# $local points at $remote and $remote is not a directory
-					ignore 'identical' $filename
+					ignore 'identical' "$filename"
 					continue
 				fi
 			else
@@ -31,11 +31,11 @@ function symlink {
 					# $remote is a real directory while
 					# $local is a directory or a symlinked directory
 					# we do not take any action regardless of which it is.
-					ignore 'identical' $filename
+					ignore 'identical' "$filename"
 					continue;
 				fi
 				if $SKIP; then
-					ignore 'exists' $filename
+					ignore 'exists' "$filename"
 					continue
 				fi
 				if ! $FORCE; then
@@ -49,15 +49,15 @@ function symlink {
 
 		if [[ ! -d $remote || -L $remote ]]; then
 			# $remote is not a real directory so we create a symlink to it
-			pending 'symlink' $filename
-			ln -s $remote $local
+			pending 'symlink' "$filename"
+			ln -s "$remote" "$local"
 		else
-			pending 'directory' $filename
-			mkdir $local
+			pending 'directory' "$filename"
+			mkdir "$local"
 		fi
 
 		success
-	done
+	done < <(find $repo/home -mindepth 1 -name .git -prune -o -print0)
 	return $EX_SUCCESS
 }
 

test/repo_fixtures.sh
@@ -122,6 +122,22 @@ EOF
 		git add .ssh
 		git commit -m 'Share known_hosts across machines'
 
+		local st3_settings="Library/Application Support/Sublime Text 3/Packages/User"
+		mkdir -p "$st3_settings"
+		cat > "$st3_settings/Preferences.sublime-settings" <<EOF
+{
+	"caret_style": "wide",
+	"default_line_ending": "unix",
+	"ensure_newline_at_eof_on_save": true,
+	"font_face": "Inconsolata-dz",
+	"rulers": [110],
+	"shift_tab_unindent": true,
+	"tab_size": 2
+}
+EOF
+		git add "$st3_settings/Preferences.sublime-settings"
+		git commit -m 'Added my Sublime Text 3 settings'
+
 		cd $dotfiles
 		git submodule add $dotfiles_vim_submodule home/.vim
 		git commit -m 'New vim configuration submodule'

commit acc8c2d0433429571279525d1d2f19d4bafe4c02

Fixes #67 (except for newlines) 5a45ce8 introduced a bug where prompts would be skipped because of process substitution in the surrounding while loop. Unit test for that bug has been added.

utils/fs.sh
@@ -9,7 +9,10 @@ function symlink {
 		ignore 'ignored' "$castle"
 		return $EX_SUCCESS
 	fi
-	while IFS= read -d $'\0' -r remote ; do
+	oldIFS=$IFS
+	IFS=$'\n'
+	for remote in $(find "$repo/home" -mindepth 1 -name .git -prune -o -print); do
+		IFS=$oldIFS
 		filename=${remote#$repo/home/}
 		local=$HOME/$filename
 
@@ -32,7 +35,7 @@ function symlink {
 					# $local is a directory or a symlinked directory
 					# we do not take any action regardless of which it is.
 					ignore 'identical' "$filename"
-					continue;
+					continue
 				fi
 				if $SKIP; then
 					ignore 'exists' "$filename"
@@ -57,7 +60,7 @@ function symlink {
 		fi
 
 		success
-	done < <(find "$repo/home" -mindepth 1 -name .git -prune -o -print0)
+	done
 	return $EX_SUCCESS
 }
 

test/suites/link.sh
@@ -26,6 +26,25 @@ function testOverwritePrompt() {
 	assertTrue "\`link' overwrote .bashrc" "[ -f $HOME/.bashrc -a ! -L $HOME/.bashrc ]"
 }
 
+function testOverwriteAnswerYes() {
+	open_bracket="\\u005b"
+	close_bracket="\\u005d"
+	esc="\\u001b$open_bracket"
+	if $EXPECT_INSTALLED; then
+		touch $HOME/.bashrc
+		cat <<EOF | expect -f - > /dev/null
+			spawn $HOMESHICK_BIN link rc-files
+			expect -ex "${esc}1;37m     conflict${esc}0m .bashrc exists\r
+${esc}1;36m   overwrite?${esc}0m ${open_bracket}yN${close_bracket}"
+			send "y\n"
+			expect EOF
+EOF
+	else
+		startSkipping
+	fi
+	assertTrue "\`link' did not overwrite .bashrc" "[ -L $HOME/.bashrc ]"
+}
+
 function testOverwriteSkip() {
 	touch $HOME/.bashrc
 	$HOMESHICK_FN --skip link rc-files > /dev/null