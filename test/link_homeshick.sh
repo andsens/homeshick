ca27542082cfbbd1e425a912142e5c1b406785c2
Anders Ingemann <anders@ingemann.de>
2014-01-11 16:59:57 +0100

    Added interactive testing shell This can be quite useful for things like completion testing etc.

--- test/link_homeshick.sh
+++ test/link_homeshick.sh
@@ -6,4 +6,5 @@ function setup_homeshick {
 	ln -s $(cd $SCRIPTDIR/..; printf "$(pwd)")/homeshick.sh $hs_repo/homeshick.sh
 	ln -s $(cd $SCRIPTDIR/../bin; printf "$(pwd)") $hs_repo/bin
 	ln -s $(cd $SCRIPTDIR/../utils; printf "$(pwd)") $hs_repo/utils
+	ln -s $(cd $SCRIPTDIR/../completions; printf "$(pwd)") $hs_repo/completions
 }
