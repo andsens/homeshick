950035f0d3ae9d1d34791d73cda59c2740b9a4b2
Anders Ingemann <anders@ingemann.de>
2014-04-23 00:09:09 +0200

    Add default setup for interactive script This makes it easier for users to try and reproduce the bug in a clean environment before reporting it.

---
+++ test/setup-default.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+printf '\nsource "$HOME/.homesick/repos/homeshick/homeshick.sh"' >> $HOME/.bashrc
+cat > $HOME/.bashrc <<EOF
+source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+source "\$HOME/.homesick/repos/homeshick/completions/homeshick-completion.bash"
+EOF
+cat > $HOME/.zshrc <<EOF
+source "\$HOME/.homesick/repos/homeshick/homeshick.sh"
+fpath=(\$HOME/.homesick/repos/homeshick/completions \$fpath)
+autoload -U compinit
+compinit
+EOF
+printf '\nalias homeshick source "$HOME/.homesick/repos/homeshick/bin/homeshick.csh"' >> $HOME/.cshrc
+mkdir -p $HOME/.config/fish
+printf '\nsource "$HOME/.homesick/repos/homeshick/homeshick.fish"' >> "$HOME/.config/fish/config.fish"
