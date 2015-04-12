146e2551346443d2fe43fe61c6413f2f9ed84234
Olav Reinert <seroton10@gmail.com>
2015-04-12 19:06:51 +0200

    Make homeshick position-independent.

--- homeshick.sh
+++ homeshick.sh
@@ -1,12 +1,15 @@
 # This script should be sourced in the context of your shell like so:
-# source $HOME/.homeshick/repos/.homeshick/homeshick.sh
+# source <path>/homeshick.sh
 # Once the homeshick() function is defined, you can type
 # "homeshick cd CASTLE" to enter a castle.
 
-function homeshick() {
-	if [ "$1" = "cd" ] && [ -n "$2" ]; then
-		cd "$HOME/.homesick/repos/$2"
+__DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+eval "function homeshick() {
+	if [ \"\$1\" = \"cd\" ] && [ -n \"\$2\" ]; then
+		cd \"\$HOME/.homesick/repos/\$2\"
 	else
-		$HOME/.homesick/repos/homeshick/bin/homeshick "$@"
+		HOMESHICK_DIR=\"$__DIR\" \"$__DIR/bin/homeshick\" \"\$@\"
 	fi
-}
+}"
+unset __DIR
+
