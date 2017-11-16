300fd8a538dda3dd211fff4a69d94fb8501be0a4
Anders Ingemann <anders@ingemann.de>
2017-11-16 17:43:58 +0100

    Like in 9d046ee rename $remote & $local in track()

--- lib/commands/track.sh
+++ lib/commands/track.sh
@@ -31,41 +31,41 @@ function track {
 	local repo="$repos/$castle"
 	oldIFS=$IFS
 	IFS=$'\n'
-	for local in $files_to_track; do
+	for homepath in $files_to_track; do
 		IFS=$oldIFS
 
-		local filepath=${local#$HOME/}
-		pending 'track' "$filepath"
-		local repo_path_remote="home/$filepath"
-		local remote="$repo/$repo_path_remote"
+		local relpath=${homepath#$HOME/}
+		pending 'track' "$relpath"
+		local relpath_in_repo="home/$relpath"
+		local repopath="$repo/$relpath_in_repo"
 
-		if [[ -e $remote ]]; then
-			ignore 'exists' "The file $filepath is already being tracked."
+		if [[ -e $repopath ]]; then
+			ignore 'exists' "The file $relpath is already being tracked."
 			continue
 		fi
 		if $check_ignore; then
-			if (cd "$repo" && git check-ignore --quiet "$repo_path_remote") then
-				ignore 'ignored' "The file $filepath would be ignored by git."
+			if (cd "$repo" && git check-ignore --quiet "$relpath_in_repo") then
+				ignore 'ignored' "The file $relpath would be ignored by git."
 				continue
 			fi
 		fi
 
-		if [[ -e $remote && $FORCE = false ]]; then
+		if [[ -e $repopath && $FORCE = false ]]; then
 			continue
-			prompt_no 'conflict' "$remote exists" "overwrite?" || continue
+			prompt_no 'conflict' "$repopath exists" "overwrite?" || continue
 		fi
 		local remote_folder
-		remote_folder=$(dirname "$remote")
+		remote_folder=$(dirname "$repopath")
 		mkdir -p "$remote_folder"
 
 		# Check if the file is a relative symlink, if so we don't move it but create
 		# an appropriate relative symlink instead that matches the new location
-		if [[ -L $local ]]; then
+		if [[ -L $homepath ]]; then
 			local target
-			target=$(readlink "$local")
+			target=$(readlink "$homepath")
 			if [[ $target =~ ^/ ]]; then
 				# It's an absolute symlink, just move it
-				mv -f "$local" "$remote"
+				mv -f "$homepath" "$repopath"
 			else
 				# Figure out the relative path from the symlink location in
 				# the castle to the path the symlink points at
@@ -73,31 +73,31 @@ function track {
 				# Convert the relative target into an absolute one
 				local abs_target
 				local target_dir
-				target_dir=$(abs_path "$(dirname "$local")")
+				target_dir=$(abs_path "$(dirname "$homepath")")
 				abs_target=$(clean_path "$target_dir/$target")
-				# Get the relative path from the remote dir to the target
-				local relpath
-				relpath=$(create_rel_path "$(dirname "$remote")" "$abs_target")
-				ln -s "$relpath" "$remote"
-				# Remove $remote so we can create the symlink further down
-				rm "$local"
+				# Get the relative path from the repopath dir to the target
+				local rel_targetpath
+				rel_targetpath=$(create_rel_path "$(dirname "$repopath")" "$abs_target")
+				ln -s "$rel_targetpath" "$repopath"
+				# Remove $homepath so we can create the symlink further down
+				rm "$homepath"
 			fi
 		else
 			# Just a regular old file. Move it
-			mv -f "$local" "$remote"
+			mv -f "$homepath" "$repopath"
 		fi
 		# Create the symlink in place of the moved file (simulate what the link command does)
-		local rel_remote
-		rel_remote=$(create_rel_path "$(dirname "$local")" "$remote")
-		ln -s "$rel_remote" "$local"
+		local rel_repopath
+		rel_repopath=$(create_rel_path "$(dirname "$homepath")" "$repopath")
+		ln -s "$rel_repopath" "$homepath"
 
 		local git_out
-		git_out=$(cd "$repo" && git add "$repo_path_remote" 2>&1)
+		git_out=$(cd "$repo" && git add "$relpath_in_repo" 2>&1)
 		status=$?
 		if [[ $status == 128 && $check_ignore == false ]]; then
 			# Currently our only option with git < 1.8.2, we can't be sure some other error hasn't occurred
-			ignore 'ignored' "The file $filepath would be ignored by git."
-			mv -f "$remote" "$local"
+			ignore 'ignored' "The file $relpath would be ignored by git."
+			mv -f "$repopath" "$homepath"
 			continue
 		elif [[ $status != 0 ]]; then
 			fail 'track' "Unable to add file to git. Git says: $git_out"
